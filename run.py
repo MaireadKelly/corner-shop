e17322b0b763004e7ec0131052f4d208ee3b1e2b
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-07-25 12:49:47 +0000

    Create get_sales_data function

--- run.py
+++ run.py
@@ -15,13 +15,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('corner-shop')
 
-sales = SHEET.worksheet('sales')
+"""
+User login, gets username from staff member.
+"""
+username = input("Please enter your username: ")
+print("Hello " + username)
 
-data = sales.get_all_values()
 
-print(data)
+def get_sales_data():
+    """ 
+    Get sales data figures from user.
+    Run a loop until a valid string of data is received from the user.
+    Valid string is 6 numbers separated by commas.
+    """
+    print("Please enter sales data for today.")
+    print("Data should be six numbers separated by commas")
 
+    data_str = input("Please enter your data:\n")
+    print(f"The data provided is {data_str}")
 
+get_sales_data()
 
 
 
