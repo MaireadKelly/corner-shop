de5e757c111c42563bdbce012f6f8d172a32e63d
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-07-26 10:38:06 +0000

    Add functionality to update sales spreadsheet

--- run.py
+++ run.py
@@ -15,39 +15,59 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('corner-shop')
 
-"""
-User login, gets username from staff member.
-"""
-username = input("Please enter your username: ")
-print("Hello " + username)
-
-def get_user_password():
-    """ 
-    Get users password to go forward to data input 
-    """
-    print("Password should be 6 digits long with NO spaces OR commas")
-        
-    data_int = input("Please enter your password: ")
-
-get_user_password()
-
-
 def get_sales_data():
-    """ 
-    Get sales data figures from user.
-    Run a loop until a valid string of data is received from the user.
-    Valid string is 6 numbers separated by commas.
     """
-    print("Please enter sales data for today.")
-    print("Data should be six numbers separated by commas")
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Please enter your data:\n")
-    
-    sales_data = data_str.split(",")
-    print(sales_data)
+        data_str = input("Enter your data here: ")
 
-get_sales_data()
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-  
+    return True
+
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
